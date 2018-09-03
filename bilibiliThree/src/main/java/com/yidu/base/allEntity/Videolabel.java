ad2e54bb1733dfa654d57e347109f190b783b224
江扬正 <2429769645@qq.com>
2018-09-03 17:09:55 +0800

    视频分类表更新

--- bilibiliThree/src/main/java/com/yidu/base/allEntity/Videolabel.java
+++ bilibiliThree/src/main/java/com/yidu/base/allEntity/Videolabel.java
@@ -9,9 +9,15 @@ public class Videolabel {
 
     private String vlName;
 
+    private String islookhome;
+
+    private String homename;
+
     private Date startTime;
 
     private Date endTime;
+
+    private String icon;
 
     public Integer getVlId() {
         return vlId;
@@ -37,6 +43,22 @@ public class Videolabel {
         this.vlName = vlName == null ? null : vlName.trim();
     }
 
+    public String getIslookhome() {
+        return islookhome;
+    }
+
+    public void setIslookhome(String islookhome) {
+        this.islookhome = islookhome == null ? null : islookhome.trim();
+    }
+
+    public String getHomename() {
+        return homename;
+    }
+
+    public void setHomename(String homename) {
+        this.homename = homename == null ? null : homename.trim();
+    }
+
     public Date getStartTime() {
         return startTime;
     }
@@ -53,6 +75,14 @@ public class Videolabel {
         this.endTime = endTime;
     }
 
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -62,8 +92,11 @@ public class Videolabel {
         sb.append(", vlId=").append(vlId);
         sb.append(", vlPid=").append(vlPid);
         sb.append(", vlName=").append(vlName);
+        sb.append(", islookhome=").append(islookhome);
+        sb.append(", homename=").append(homename);
         sb.append(", startTime=").append(startTime);
         sb.append(", endTime=").append(endTime);
+        sb.append(", icon=").append(icon);
         sb.append("]");
         return sb.toString();
     }
--- bilibiliThree/src/main/java/com/yidu/base/allMapper/VideolabelMapper.java
+++ bilibiliThree/src/main/java/com/yidu/base/allMapper/VideolabelMapper.java
@@ -1,11 +1,8 @@
 package com.yidu.base.allMapper;
 
 import com.yidu.base.allEntity.Videolabel;
-
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-@Repository
 public interface VideolabelMapper {
     int deleteByPrimaryKey(Integer vlId);
 
